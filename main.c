4c81980fa34bba3f16cd2d1531b8dda6e6c90816
Your Name <you@example.com>
2018-12-10 03:35:41 +0100

    dodao ispis-Game Over,ukljuceno koriscenje strelica,implementirano skakanje

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ static void left_wall(void);
 static void rigth_wall(void);
 static void game_over_hole(void);
 static void specialKeys(int key, int x, int y);
+static void finish(void);
 
 int width_window = 700;
 int height_window = 500;
@@ -215,6 +216,8 @@ static void on_display(void){
         game_over_hole();
   }
   
+  finish();
+  
   glutSwapBuffers();
 }
 
@@ -363,6 +366,52 @@ void check_hole(float xl,float xd,float zg,float zd){
        animation_ongoing = 0;
      }
 }
+
+void finish(void) {
+    
+    if(202.6 <= z_ball - z_1){
+        char ispis1[8];
+        char ispis2[8];
+        char ispis3[8];
+        char ispis4[8];
+        sprintf(ispis1, "Happy");
+        sprintf(ispis2, "New");
+        sprintf(ispis3, "Year");
+        sprintf(ispis4, "2019");
+        
+        glPushMatrix();
+            glColor3d(1,0,0);
+            glRasterPos3f(0.5,0,2);
+            int len1 = strlen(ispis1);
+            for(int i =0; i<len1; i++) {
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ispis1[i]);
+            }
+            
+            glColor3d(0,1,0);
+            glRasterPos3f(0,0,2);
+            int len2 = strlen(ispis2);
+            for(int i =0; i<len2; i++) {
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ispis2[i]);
+            }
+            
+            glColor3d(0,0,1);
+            glRasterPos3f(-0.4,0,2);
+            int len3 = strlen(ispis3);
+            for(int i =0; i<len3; i++) {
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ispis3[i]);
+            }
+            
+            glColor3d(1,1,0);
+            glRasterPos3f(0,0,1.5);
+            int len4 = strlen(ispis4);
+            for(int i =0; i<len4; i++) {
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ispis4[i]);
+            }
+        glPopMatrix();
+        animation_ongoing = 0;
+    }
+}
+
 
 // isrtavanje desnog zida
 static void rigth_wall(void){
